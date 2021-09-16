d9263e2bcec1062d4cebe53fc2a253f8734ef3ec
ninhdochi <76436328+ninhdochi@users.noreply.github.com>
2021-09-17 02:04:01 +0700

    Update providers.tf

--- modules/eks/providers.tf
+++ modules/eks/providers.tf
@@ -6,10 +6,6 @@ terraform {
     }
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
